9999bf7e07cbae0e999b669337bc0551ee370067
Dejan Katovic <dejankatovic@gmail.com>
2023-12-22 17:48:55 +0100

    fixdeploy

--- src/pages/home/transaction/index.jsx
+++ src/pages/home/transaction/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import { CustomDivder } from "../components/divider";
 import { MainTitle } from "../components/mainTitle";
 import { CustomContainer } from "../../../CustomUI/customContainer";
